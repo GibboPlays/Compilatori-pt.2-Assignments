d71a5ad9cf6a9d3dd06cd2f5fc348613fb3e8bbf
GibboPlays <simone.gibellini@virgilio.it>
2025-04-28 19:59:42 +0200

    trovare loop-invariant con reaching definitions

--- 3°assignment/loop-invariant_code_motion/Loop-Invariant_Code_Motion_opts.cpp
+++ 3°assignment/loop-invariant_code_motion/Loop-Invariant_Code_Motion_opts.cpp
@@ -28,11 +28,12 @@
 #include "llvm/IR/Module.h"
 #include "llvm/Analysis/LoopInfo.h"
 #include "llvm/IR/Dominators.h"
+#include "llvm/IR/CFG.h"
 #include "llvm/ADT/SmallVector.h"
-#include "llvm/ADT/BitVector.h"
 #include <cmath>
 #include <vector>
 #include <map>
+#include <set>
 
 using namespace llvm;
 
@@ -47,25 +48,80 @@ namespace {
 // New PM implementation
 struct TestPass: PassInfoMixin<TestPass> {
 
-  std::map<BasicBlock*,std::pair<BitVector,BitVector>> getReachingDefinitions(Function &F)
+  std::map<BasicBlock*,std::pair<std::set<Instruction*>,std::set<Instruction*>>> getReachingDefinitions(Function &F)
   {
-    std::map<BasicBlock*,std::pair<BitVector,BitVector>> rd;
+    std::map<BasicBlock*,std::pair<std::set<Instruction*>,std::set<Instruction*>>> rd;
     unsigned int numInst = F.getInstructionCount();
-    BitVector in(numInst), out(numInst);
-    std::pair<BitVector,BitVector> p(in,out);
+    std::set<Instruction*> in, out;
+    std::pair<std::set<Instruction*>,std::set<Instruction*>> p(in,out);
     std::vector<BasicBlock*> changedNodes;
 
     //Initialize
     for (BasicBlock &bb : F)
     {
       rd[&bb]=p;
-      changedNodes.append(&bb);
+      changedNodes.push_back(&bb);
     }
+
+    // Condizione al contorno: out[entry] = ∅
+    BasicBlock *entry = &F.getEntryBlock();
+    rd[entry].second.clear(); // out[entry] = ∅
 
     //Iterate
     while(!changedNodes.empty())
     {
-      
+      BasicBlock &bi = *(changedNodes.front());
+      changedNodes.pop_back();
+
+      std::set<Instruction*> ini; //in[i]
+      //Scorro i predecessori
+      for (BasicBlock *pred :  predecessors(&bi)) 
+      {
+        std::set outp = (rd.at(pred)).second; //out[p]
+        ini.insert(outp.begin(), outp.end()); //in[i] = U out[p]
+      }
+      rd[&bi].first = ini;
+
+      //Genero gen[i] e kill[i]
+      std::set<Instruction*> geni, killi;
+      for (Instruction &inst : bi)
+      {
+        if (inst.isBinaryOp())
+        {
+          geni.insert(&inst);
+        }
+        for (Instruction *instPtr : ini)
+        {
+          Instruction &instAdd = *instPtr;
+          if (&inst == &instAdd)
+          {
+            killi.insert(&instAdd);
+          }
+        }
+      }
+
+      //Calcolo out[i]=gen[i]U(in[i]-kill[i]) 
+      std::set<Instruction*> in_minus_kill;
+      std::set_difference(ini.begin(), ini.end(),
+                          killi.begin(), killi.end(),
+                          std::inserter(in_minus_kill, in_minus_kill.begin()));
+        
+      std::set<Instruction*> outi;
+      outi.insert(geni.begin(), geni.end());
+      outi.insert(in_minus_kill.begin(), in_minus_kill.end());
+
+      //Controllo se out[i] è cambiato
+      if (rd[&bi].second != outi)
+      {
+        rd[&bi].second = outi;
+        for (BasicBlock *succ : successors(&bi))
+        {
+          if (std::find(changedNodes.begin(), changedNodes.end(), succ) == changedNodes.end()) 
+          {
+            changedNodes.push_back(succ);
+          }
+        }
+      }
     }
 
 
@@ -73,81 +129,68 @@ struct TestPass: PassInfoMixin<TestPass> {
 
   }
 
-  std::vector<std::pair<Instruction*,Loop*>> getLoopInvariants(LoopInfo &LI)
+  std::vector<std::pair<Instruction*,Loop*>> getLoopInvariants(LoopInfo &LI, 
+                                                              std::map<BasicBlock*,std::pair<std::set<Instruction*>,std::set<Instruction*>>> rd)
   {
     //Vettore di coppie istruzioni Loop-Invariant e loop a cui appartiene
     std::vector<std::pair<Instruction*,Loop*>> LII;
-    //Per capire se l'istruzione usata è loop invariant
-    bool li1;
-    //Per capire se l'istruzione che usa è loop invariant
-    bool li2;
 
-    //Vettore dei definiti non Loop-Invariant
-    std::vector<Instruction*> defined;
     for (Loop *L: LI)
     {
       for (BasicBlock *BB : L->blocks()) 
       {
-          
-        for (auto It = BB->begin(); It != BB->end(); ++It) 
+        const auto BBrd = rd.at(BB).first; //in[BB]
+
+        for (Instruction& Inst : *BB)
         {
-          Instruction &Inst = *It;
+          bool isLoopInvariant = true;
 
-          li1 = true;
-
-          for (auto Iter = Inst.user_begin(); Iter != Inst.user_end(); ++Iter)
-          {
-            Instruction &InstAdd = *(dyn_cast<Instruction>(*Iter));
-
-            li2 = true;
-
-            //Se l'istruzione che usa è già in defined è inutile continuare
-            for (Instruction *i: defined)
-            {
-              if (&InstAdd == i)
-              {
-                li2 = false;
-                break;
-              }
-            }
+          for (auto *Iter = Inst.op_begin(); Iter != Inst.op_end(); ++Iter) {
+            Value *Operand = *Iter;
             
-            if (li2)
+            //Se l'operando è un'istruzione
+            if (Instruction* opInst = dyn_cast<Instruction>(Operand))
             {
-              //Guardo se l'istruzione di cui sto cercando gli usi è in defined
-              for (Instruction *i: defined)
+              if (L->contains(opInst->getParent())) 
               {
-                if (&Inst == i)
+                //Verifico se è loop-invariant (deve essere già nella lista)
+                bool found = false;
+                for (const auto &pair : LII) 
                 {
-                  li1 = false;
-                  li2 = false;
-                  defined.push_back(&InstAdd);
-                  break;
+                    if (pair.first == opInst && pair.second == L) {
+                        found = true;
+                        break;
+                    }
+                }
+                
+                if (!found) 
+                {
+                    isLoopInvariant = false;
+                    break;
+                }
+              } 
+              //Se la definizione viene da fuori il loop, verifica che sia nelle reaching definitions
+              else 
+              {
+                if (BBrd.find(opInst) == BBrd.end()) 
+                {
+                    isLoopInvariant = false;
+                    break;
                 }
               }
+            } else { //Se non è una istruzione
+              isLoopInvariant = false;
+              break;
             }
-            
-            //Se ha passato non è in defined può essere candidata a essere loop invariant
-            if (li2 && L->contains(InstAdd.getParent()))
-            {
-              std::pair<Instruction*,Loop*> p1(&InstAdd,L);
-              LII.push_back(p1);
-            }
-
-            
+          
           }
 
-          if (li1)
-          {
-            std::pair <Instruction*,Loop*> p(&Inst,L);
-            LII.push_back(p);
-          } 
+          if (isLoopInvariant) {
+            LII.emplace_back(&Inst, L);
+          }
         }
-    
       }
-
-      defined.clear();
     }
-
     return LII;
   }
 
@@ -235,24 +278,10 @@ struct TestPass: PassInfoMixin<TestPass> {
 
     DominatorTree &DT = AM.getResult<DominatorTreeAnalysis>(F);
 
-    //Debug
-    std::map<BasicBlock*,std::pair<BitVector,BitVector>> rd = getReachingDefinitions(F);
-    for (auto [bb,p]: rd)
-    {
-      outs() << bb->getName() << ": ";
-      for (unsigned i = 0; i < p.first.size(); ++i) {
-        outs() << (p.first.test(i) ? "1" : "0");
-      }
-      outs() << " ";
-      for (unsigned i = 0; i < p.second.size(); ++i) {
-        outs() << (p.second.test(i) ? "1" : "0");
-      }
-      outs() << "\n";
-    }
-
+    std::map<BasicBlock*,std::pair<std::set<Instruction*>,std::set<Instruction*>>> rd = getReachingDefinitions(F);
     
     //Istruzioni Loop-Invariant
-    std::vector<std::pair<Instruction*,Loop*>> LII = getLoopInvariants(LI);
+    std::vector<std::pair<Instruction*,Loop*>> LII = getLoopInvariants(LI,rd);
 
     //Scorro le coppie dei loop invariant
     for (std::pair<Instruction*,Loop*> *p : LII)
@@ -278,7 +307,6 @@ struct TestPass: PassInfoMixin<TestPass> {
 
     }
 
-    
   	return PreservedAnalyses::all();
 }
 
